566ec316ce6eee32fd0bf48dc8e4ee0badab5c71
Jeremias ramirez <jereramirez499@gmail.com>
2020-02-13 11:31:46 -0400

    agregando sistema de ruta

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from "@angular/forms"
+
+
 
 import { AppComponent } from './app.component';
+import {  HomeComponent } from "./home/home.component"
+import {  InitComponent } from "./init/init.component"
+
+
+import {  routingMovie } from "./app.route"
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    InitComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    routingMovie
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/app.route.ts
@@ -0,0 +1,17 @@
+import { Routes, RouterModule } from "@angular/router";
+import { ModuleWithProviders } from "@angular/core"
+
+
+// components
+
+import { HomeComponent } from "./home/home.component"
+import { InitComponent } from "./init/init.component"
+
+
+const ROUTES : Routes = [
+  {path: "home", component: HomeComponent},
+  {path: '', component: InitComponent}
+]
+
+
+export var routingMovie : ModuleWithProviders = RouterModule.forRoot(ROUTES);
