0be1b5561a03baecb835fd9be9fcce9561471192
Jeremias ramirez <jereramirez499@gmail.com>
2020-02-13 20:21:05 -0400

    slider creado!

--- src/app/init/init.component.ts
+++ src/app/init/init.component.ts
@@ -11,11 +11,58 @@ import { Router } from "@angular/router"
 
 export class InitComponent{
 
+  //properties
+  public sliderRoutes : any[] = ["../assets/poster/posterMovie.jpg"]
+  public allRoutesSlider : any[] = []
 
   constructor(public navigate : Router){
+    this.pushBankImage();
+    this.sliderInit();
 
+
+      setTimeout(()=>{
+        this.scrollTo();
+      },2000)
+
+
+  }
+
+  resetSlider(){
+    this.sliderRoutes.unshift("../assets/poster/posterMovie.jpg");
     setTimeout(()=>{
-      this.scrollTo();
+      this.sliderInit();
+    },1500);
+  }
+
+  // functions
+  pushBankImage(){
+
+    return  this.allRoutesSlider = [
+        "./assets/poster/poster1.jpeg",
+        "./assets/poster/poster2.jpg",
+        "./assets/poster/poster3.jpg",
+        "./assets/poster/poster4.jpg",
+        "./assets/poster/poster5.jpg",
+        "./assets/poster/poster6.jpg"
+      ]
+
+  }
+  sliderInit(){
+
+
+    let counterSlider :number= 0
+    let intervalSlider = setInterval(()=>{
+
+        this.sliderRoutes.unshift(this.allRoutesSlider[counterSlider])
+
+        if (counterSlider == 6 ){
+            this.resetSlider();
+            clearInterval(intervalSlider);
+
+        }
+
+        counterSlider++;
+
     },2000)
 
 
@@ -23,19 +70,19 @@ export class InitComponent{
   }
 
   scrollTo(){
-      let counterInterval: number = 200;
-      let intervalScroll = setInterval(()=>{
-          counterInterval -= 5;
+      let counterInterval: number = 100;
+       setTimeout(()=>{
 
           window.scrollBy(0, counterInterval)
-         
-          if (counterInterval === 0){
-            clearInterval(intervalScroll);
-          }
+
       },100);
   }
+
+
+
   navigateToHome(){
     this.navigate.navigate(["home"]);
   }
+
 
 }
