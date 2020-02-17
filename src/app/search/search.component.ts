e56c865e3f3eb3a2503960c846d863d4b847af80
Jeremias ramirez <jereramirez499@gmail.com>
2020-02-16 23:51:22 -0400

    preparando para mostrar resultado de busqueda

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,25 +1,37 @@
 import { Component } from "@angular/core";
 import { Router, ActivatedRoute } from "@angular/router"
+import { Catalogs } from "../services/service.catalogs"
 
 @Component({
   selector: 'search-app',
   templateUrl: './search.component.html',
-  styleUrls: ['./search.component.css', '../home/home.component.css']
+  styleUrls: ['./search.component.css', '../home/home.component.css'],
+  providers: [Catalogs]
 })
 
 export class SearchComponent{
   public showCard: number = 0;
-  public existParam :number= 0;
+  public param:Object;
+  public textInput : any = "";
 
-
-
-  constructor(public router:Router, public param:ActivatedRoute){
+  constructor(public router:Router, public param:ActivatedRoute, public catalogs:Catalogs){
     setTimeout(()=>{this.showCard = 1}, 1000);
 
     this.param.params.subscribe((params)=>{
-      if (!(params === {})){
-          this.existParam=1;
-          console.log(params)
+      this.param = params;
+
+
+
+      console.log(this.param)
+
+
+      if (this.param.result){
+        this.textInput = this.param.result
+        this.searchShow(this.textInput)
+        console.log("esta lleno")
+      }
+      else{
+        console.log("esta vacio")
       }
 
     })
@@ -29,4 +41,18 @@ export class SearchComponent{
   redirectToNavigate(){
     this.router.navigate(["navigate"])
   }
+
+  searchShow(response:any){
+    console.log(response)
+    if (response != ""){
+      this.router.navigate(["search", response])
+
+      this.catalogs.getCatalogs(response).subscribe((data)=>{
+        console.log(data)
+      })
+
+    }
+
+  }
+
 }
